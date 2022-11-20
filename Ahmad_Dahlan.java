5c4b195d38c6bc3893904dad9d3cbd3ce742872f
faiqahferaristaa <114296349+faiqahferaristaa@users.noreply.github.com>
2022-11-21 01:14:46 +0800

    Add files via upload

---
+++ Ahmad_Dahlan.java
@@ -0,0 +1,14 @@
+package com.f55121112.mylistview1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ahmad_Dahlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ahmad_dahlan);
+    }
+}
---
+++ Ahmad_Yani.java
@@ -0,0 +1,14 @@
+package com.f55121112.mylistview1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ahmad_Yani extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ahmad_yani);
+    }
+}
